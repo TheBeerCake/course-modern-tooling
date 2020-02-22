93ad6f1b7050b6e82515656ce1a4bf8b40cd852f
TheBeerCake <kristaps.klevers@gmail.com>
2020-02-22 21:56:39 +0200

    Add react lazy load example

--- webpack.config.base.js
+++ webpack.config.base.js
@@ -24,7 +24,8 @@ module.exports = {
           }], '@babel/preset-react'],
           plugins: [
               "@babel/plugin-proposal-class-properties",
-              "react-hot-loader/babel"
+              "@babel/plugin-syntax-dynamic-import",
+              "react-hot-loader/babel",
             ]
         }
       },
