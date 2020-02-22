435bc432a328757320cfbff5a05b39ca42515dea
TheBeerCake <kristaps.klevers@gmail.com>
2020-02-22 22:38:36 +0200

    Add jest testing example and prettier formater

--- webpack.config.base.js
+++ webpack.config.base.js
@@ -1,42 +1,29 @@
-const path = require('path')
-const HtmlWebpackPlugin = require('html-webpack-plugin')
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
 
 module.exports = {
-  entry: './src/index.js',
-  output: {
-    path: path.join(__dirname, 'dist'),
-    filename: 'app.bundle.js'
-  },
-  module: {
-    rules: [
-      {
-        test: /\.js$/,
-        loader: "babel-loader",
-        exclude: /node_modules/,
-        options: {
-          presets: [["@babel/preset-env", {
-              targets: [
-                  "last 2 versions",
-                  "not dead",
-                  "not < 2%" ,
-              ],
-              useBuiltIns: "entry"
-          }], '@babel/preset-react'],
-          plugins: [
-              "@babel/plugin-proposal-class-properties",
-              "@babel/plugin-syntax-dynamic-import",
-              "react-hot-loader/babel",
-            ]
-        }
-      },
-      {
-        test: /\.css$/,
-        use:["style-loader", "css-loader"],
-        exclude: /node_modules/,
-      }
+    entry: "./src/index.js",
+    output: {
+        path: path.join(__dirname, "dist"),
+        filename: "app.bundle.js"
+    },
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                loader: "babel-loader",
+                exclude: /node_modules/
+            },
+            {
+                test: /\.css$/,
+                use: ["style-loader", "css-loader"],
+                exclude: /node_modules/
+            }
+        ]
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: "./src/index.html"
+        })
     ]
-  },
-  plugins: [new HtmlWebpackPlugin({
-    template: "./src/index.html"
-  })]
-}
+};
